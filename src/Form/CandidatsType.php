22af03d9286c3e60589d8fd4e79a1faa5e71b2b5
Julie J <jeannet.julie@gmail.com>
2022-03-09 15:59:35 +0100

    Personnalisation des champs avec les FormTypes

--- src/Form/CandidatsType.php
+++ src/Form/CandidatsType.php
@@ -2,11 +2,16 @@
 
 namespace App\Form;
 
+use App\Entity\Hobbies;
+use App\Entity\Profiles;
 use App\Entity\Candidats;
+use Doctrine\ORM\EntityRepository;
+use App\Repository\HobbiesRepository;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CandidatsType extends AbstractType
 {
@@ -18,8 +23,20 @@ class CandidatsType extends AbstractType
             ->add('age')
             // ->add('createdAt')
             // ->add('updatedAt')
-            ->add('profiles')
-            ->add('hobbies')
+            ->add('profiles', EntityType::class,[
+                'expanded'=>true,
+                'class'=>Profiles::class, 
+                'multiple'=>false,
+            ])
+            ->add('hobbies', EntityType::class, [
+                'expanded'=>true,
+                'class'=>Hobbies::class,
+                'multiple'=>true,
+                'query_builder'=>function(HobbiesRepository $repo){
+                    return $repo->createQueryBuilder('h')
+                    ->orderBy('h.designation', 'ASC');
+                }
+            ])
             ->add('jobs')
             ->add('editer', SubmitType::class)
         ;
